7f72215369f153184353b613719a8c93285f6c9a
Cruse, Si <si_cruse@troweprice.com>
2018-10-26 12:41:31 -0400

    Initial working media configuration

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Setting up..."
+
+############ Invoke Ansible ############
+echo Refreshing Ansible Galaxy...
+ansible-galaxy install -f -r requirements.yml
+echo Invoking Ansible...
+ansible-playbook -i inventory.yml setup.yml --ask-become-pass
+
+echo "Setup complete..."
